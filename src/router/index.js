b35c43b6195eaf2d0b649dfd9ecda9da3586782d
Atoti2 <bogyikormi3@gmail.com>
2023-11-13 14:57:16 +0100

    bug

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,6 @@ import Projects from '../views/Projects.vue'
 import Contact from '../views/Contact.vue'
 import About from '../views/About.vue'
 const router = createRouter({
- 
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
