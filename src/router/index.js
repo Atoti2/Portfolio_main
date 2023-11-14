cb2ea9bb696621e6f2aad276accbe8adc1cadbec
Atoti2 <bogyikormi3@gmail.com>
2023-11-14 19:44:02 +0100

    scripts added

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Home from '../views/Home.vue'
 import Projects from '../views/Projects.vue'
 import Contact from '../views/Contact.vue'
 import About from '../views/About.vue'
+
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
