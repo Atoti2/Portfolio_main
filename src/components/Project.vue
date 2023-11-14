2aac8d84ff1455f141da7e3a8e8447fb530517ae
Atoti2 <bogyikormi3@gmail.com>
2023-11-14 16:46:41 +0100

    Project component

---
+++ src/components/Project.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="relative hover:scale-105 transition-all cursor-pointer h-80 w-80 p-5 ">
+        <img class="bg-cover rounded-md" :src="getImageUrl()" alt="">
+        <h3 class="absolute left-6 top-5 text-xl font-bold">{{ title }}</h3>
+        <p class="absolute left-6 bottom-9 font-mono backdrop-blur-lg rounded-tr rounded-br w-3/4">{{ desc }}</p>
+    </div>
+</template>
+
+<script setup>
+import {defineProps} from "vue";
+
+     const props =  defineProps({
+      title: String,
+      image: String,
+      desc: String
+   })
+
+   function getImageUrl() {
+  return new URL(`../assets/${props.image}`, import.meta.url)
+}
+</script>
+<style>
+
+</style>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -2,22 +2,26 @@
    <div class="p-12 bg-rose-500 m-6 rounded-lg ">
                 <h2 id="projektek" class="font-mono font-bold text-3xl mb-5 text-slate-100">Projektek</h2>
                 <div class="flex gap-12 flex-wrap items-center lg:justify-normal justify-center">
-                    <div class="relative hover:scale-105 transition-all cursor-pointer h-80 w-80 bg-cover bg-[url(https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE2s1hE?ver=8101)] rounded-md p-5">
-                        <h3 class="absolute left-5 top-5 text-xl font-bold">Projekt 1</h3>
-                        <p class="absolute left-5 bottom-5 font-mono backdrop-blur-xl rounded-md">Rövid leirás a projektről.</p>
-                    </div>
-                    <div class="relative hover:scale-105 transition-all cursor-pointer h-80 w-80 bg-cover bg-[url(https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE2s1hE?ver=8101)] rounded-md p-5">
-                        <h3 class="absolute left-5 top-5 text-xl font-bold">Projekt 1</h3>
-                        <p class="absolute left-5 bottom-5 font-mono backdrop-blur-xl rounded-md">Rövid leirás a projektről.</p>
+                    <div v-for="project in projects" :key="project.title">
+                        <Project
+                            :title="project.title"
+                            :image="project.image"
+                            :desc="project.desc"
+                        />
                     </div>
                 </div>
             </div>
 </template>
 
-<script>
-export default {
-
-}
+<script setup>
+    import Project from '../components/Project.vue';
+    const projects = [
+        {
+            title: 'Számológép',
+            image: 'profile_picture.png',
+            desc: 'Egy számológép Javascriptben csinálva.',
+        }
+    ]
 </script>
 
 <style>
